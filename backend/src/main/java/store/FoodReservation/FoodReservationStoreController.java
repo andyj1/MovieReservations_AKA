9a064907aa7efe8499b5b5c116933f55e7b934a2
Ali Rahman <alirahman1117@gmail.com>
2019-12-14 22:52:33 -0500

    sync

--- backend/src/main/java/store/FoodReservation/FoodReservationStoreController.java
+++ backend/src/main/java/store/FoodReservation/FoodReservationStoreController.java
@@ -2,25 +2,21 @@ package store.FoodReservation;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.bson.Document;
-import store.Showtime.ShowtimeStore;
-import store.Showtime.ShowtimeStoreController;
-import store.User.UserStore;
-import store.User.UserStoreController;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class FoodReservationStoreController implements FoodReservationStore {
 
     Config config = ConfigFactory.load("AKA.conf");
     MongoClient dbClient = new MongoClient(new MongoClientURI(config.getString("mongo.uri")));
     MongoDatabase database = dbClient.getDatabase(config.getString("mongo.database"));
-    MongoCollection<Document> foodReservationCollection = database.getCollection(config.getString("mongo.collection_food_reservations"));
-
-    UserStore us = new UserStoreController();
-    ShowtimeStore ss = new ShowtimeStoreController();
+    MongoCollection<Document> foodReservationCollection = database.getCollection(this.config.getString("mongo.collection_collection_food_reservations"));
 
     public FoodReservationStoreController() {
         this.config = config;
@@ -28,21 +24,57 @@ public class FoodReservationStoreController implements FoodReservationStore {
 
     @Override
     public Integer getCount(String id) {
-        return null;
+        try{
+            Document doc = foodReservationCollection.find(eq("_id", id)).first();
+            if (!doc.isEmpty()) {
+                return doc.getInteger("count");
+            }
+            return 0;
+        } catch (MongoException e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
     @Override
     public String getUserId(String id) {
-        return null;
+        try{
+            Document doc = foodReservationCollection.find(eq("user_id", id)).first();
+            if (!doc.isEmpty()) {
+                return doc.getString("user_id");
+            }
+            return null;
+        } catch (MongoException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public String getShowtimeId(String id) {
-        return null;
+        try{
+            Document doc = foodReservationCollection.find(eq("showtime_id", id)).first();
+            if (!doc.isEmpty()) {
+                return doc.getString("showtime_id");
+            }
+            return null;
+        } catch (MongoException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public String getFoodId(String id) {
-        return null;
+        try{
+            Document doc = foodReservationCollection.find(eq("food_id", id)).first();
+            if (!doc.isEmpty()) {
+                return doc.getString("food_id");
+            }
+            return null;
+        } catch (MongoException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }

a55668125655f738785b8a411eb20e84367028bd
kevinlin6543 <kevin.lin6543@gmail.com>
2019-12-14 22:48:25 -0500

    added fetch requests

--- frontend/src/signup.js
+++ frontend/src/signup.js
@@ -1,30 +1,131 @@
-import React from 'react';
-import {Button, Card, Form} from "react-bootstrap";
+import React, {Component} from 'react';
+import {Button, Card, Form, Col, InputGroup} from "react-bootstrap";
+import loggedIn from './loggedIn'
 
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      validated: '',
+      setValidated: '',
+    };
+  }
+  handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
 
-function signup() {
-  return (
-    <>
-    <div style={{paddingTop: '3vh'}}/>
-      <Card style={{ width: '20vw', marginLeft: 'auto', marginRight: 'auto'}}>
-      <Form style={{ width: '18vw', marginLeft: 'auto', marginRight: 'auto', paddingTop: '2vh', paddingBottom: '2vh'}}>
-        <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+    }
+    event.preventDefault();
+    fetch('http://192.168.1.158:1010/signup?username=' + form.elements.validationCustomUsername.value
+      + '&password=' + form.elements.validationPassword.value
+      + '&name=' + form.elements.validationName.value
+      + '&email=' + form.elements.validationEmail.value
+      + '&genre=' + form.elements.validationGenre.value
+      + '&zip_code=10009' + form.elements.validationZIP.value
+      + '&admin=' + form.elements.adminStatus.checked, {
+      method: 'GET'
+    }).then(response => {
+        if(response.status === 200) {
+          localStorage.setItem('user', form.elements.validationCustomUsername.value);
+        }
+      });
+    this.setState({setValidated: true});
+  };
 
-        <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-        Submit
-        </Button>
-      </Form>
-      </Card>
-    </>
-  );
+  componentDidMount() {
+    if (loggedIn()) {
+      this.props.history.push('/Movies');
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div style={{paddingTop: '3vh'}}/>
+        <Card style={{width: '30vw', marginLeft: 'auto', marginRight: 'auto'}}>
+          <h2 style={{marginLeft: 'auto', marginRight: 'auto', marginTop: '2vh'}}> Sign Up </h2>
+          <Form noValidate validated={this.state.validated} onSubmit={(e) => this.handleSubmit(e)} style={{
+            width: '28vw',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            paddingTop: '2vh',
+            paddingBottom: '2vh'
+          }}>
+            <Form.Row>
+              <Form.Group as={Col} md="6" name="name" controlId="validationName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Enter name"
+                />
+                <Form.Control.Feedback></Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="6" name="username" controlId="validationCustomUsername">
+                <Form.Label>Username</Form.Label>
+                <InputGroup>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <Form.Control
+                    type="text"
+                    placeholder="Username"
+                    aria-describedby="inputGroupPrepend"
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please choose a username.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md="6" name="email" controlId="validationEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Email" required/>
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid Email
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="6" name="password" controlId="validationPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" required/>
+                <Form.Control.Feedback type="invalid">
+                  Please enter a Password
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md="6" name="genre" controlId="validationGenre">
+                <Form.Label>Favorite Genre</Form.Label>
+                <Form.Control type="text" placeholder="Your genre" required/>
+                <Form.Control.Feedback type="invalid">
+                  Please enter a genre
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="6" name="zip" controlId="validationZIP">
+                <Form.Label>Zip Code</Form.Label>
+                <Form.Control type="password" placeholder="ZIP Code" required/>
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid zip.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Group name="adminstatus" controlId="adminStatus">
+              <Form.Check
+                label="Admin Account"
+              />
+            </Form.Group>
+            <Button type="submit">Sign Up</Button>
+          </Form>
+        </Card>
+      </>
+    );
+  }
 }
 
-export default signup;
+export default Signup;
 
